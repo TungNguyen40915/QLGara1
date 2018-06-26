d4a991df7066f5b95f5984103f9d0e9d1234280b
ynctl <ynctl@DESKTOP-F4PFVVV>
2018-06-26 14:43:34 +0700

    new

--- QLGaraGUI/tiepnhanxe.Designer.vb
+++ QLGaraGUI/tiepnhanxe.Designer.vb
@@ -46,7 +46,6 @@ Partial Class tiepnhanxe
         Me.btthoat = New System.Windows.Forms.Button()
         Me.tbsolan = New System.Windows.Forms.TextBox()
         Me.labelsolan = New System.Windows.Forms.Label()
-        Me.Label10 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
         Me.Panel3.SuspendLayout()
@@ -54,60 +53,65 @@ Partial Class tiepnhanxe
         '
         'Panel1
         '
-        Me.Panel1.Controls.Add(Me.Label10)
         Me.Panel1.Controls.Add(Me.Label9)
         Me.Panel1.Controls.Add(Me.tbhieuxe)
         Me.Panel1.Controls.Add(Me.Label4)
         Me.Panel1.Controls.Add(Me.tbbiensoxe)
         Me.Panel1.Controls.Add(Me.Label3)
-        Me.Panel1.Location = New System.Drawing.Point(29, 135)
+        Me.Panel1.Location = New System.Drawing.Point(19, 88)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(998, 91)
+        Me.Panel1.Size = New System.Drawing.Size(665, 59)
         Me.Panel1.TabIndex = 0
         '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(370, 34)
+        Me.Label9.Location = New System.Drawing.Point(247, 22)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(78, 20)
+        Me.Label9.Size = New System.Drawing.Size(54, 13)
         Me.Label9.TabIndex = 4
         Me.Label9.Text = "Thêm xe?"
         '
         'tbhieuxe
         '
         Me.tbhieuxe.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbhieuxe.Location = New System.Drawing.Point(609, 21)
+        Me.tbhieuxe.Location = New System.Drawing.Point(406, 14)
+        Me.tbhieuxe.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbhieuxe.Name = "tbhieuxe"
         Me.tbhieuxe.ReadOnly = True
-        Me.tbhieuxe.Size = New System.Drawing.Size(199, 39)
+        Me.tbhieuxe.Size = New System.Drawing.Size(134, 29)
         Me.tbhieuxe.TabIndex = 3
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(505, 30)
+        Me.Label4.Location = New System.Drawing.Point(337, 19)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(78, 25)
+        Me.Label4.Size = New System.Drawing.Size(55, 17)
         Me.Label4.TabIndex = 2
         Me.Label4.Text = "Hiệu xe"
         '
         'tbbiensoxe
         '
         Me.tbbiensoxe.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbbiensoxe.Location = New System.Drawing.Point(153, 21)
+        Me.tbbiensoxe.Location = New System.Drawing.Point(102, 14)
+        Me.tbbiensoxe.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbbiensoxe.Name = "tbbiensoxe"
-        Me.tbbiensoxe.Size = New System.Drawing.Size(199, 39)
+        Me.tbbiensoxe.Size = New System.Drawing.Size(134, 29)
         Me.tbbiensoxe.TabIndex = 1
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(15, 30)
+        Me.Label3.Location = New System.Drawing.Point(10, 19)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(103, 25)
+        Me.Label3.Size = New System.Drawing.Size(73, 17)
         Me.Label3.TabIndex = 0
         Me.Label3.Text = "Biển số xe"
         '
@@ -117,18 +121,20 @@ Partial Class tiepnhanxe
         Me.Panel2.Controls.Add(Me.dtngaytiepnhan)
         Me.Panel2.Controls.Add(Me.Label1)
         Me.Panel2.Controls.Add(Me.tbmatiepnhan)
-        Me.Panel2.Location = New System.Drawing.Point(29, 36)
+        Me.Panel2.Location = New System.Drawing.Point(19, 23)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(1036, 83)
+        Me.Panel2.Size = New System.Drawing.Size(691, 54)
         Me.Panel2.TabIndex = 1
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(3, 24)
+        Me.Label2.Location = New System.Drawing.Point(2, 16)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(228, 37)
+        Me.Label2.Size = New System.Drawing.Size(153, 26)
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "Mã  Tiếp Nhận"
         '
@@ -136,28 +142,31 @@ Partial Class tiepnhanxe
         '
         Me.dtngaytiepnhan.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.dtngaytiepnhan.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.dtngaytiepnhan.Location = New System.Drawing.Point(792, 24)
+        Me.dtngaytiepnhan.Location = New System.Drawing.Point(528, 16)
+        Me.dtngaytiepnhan.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.dtngaytiepnhan.Name = "dtngaytiepnhan"
-        Me.dtngaytiepnhan.Size = New System.Drawing.Size(241, 39)
+        Me.dtngaytiepnhan.Size = New System.Drawing.Size(162, 29)
         Me.dtngaytiepnhan.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(524, 24)
+        Me.Label1.Location = New System.Drawing.Point(349, 16)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(250, 37)
+        Me.Label1.Size = New System.Drawing.Size(168, 26)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Ngày Tiếp Nhận"
         '
         'tbmatiepnhan
         '
         Me.tbmatiepnhan.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbmatiepnhan.Location = New System.Drawing.Point(242, 24)
+        Me.tbmatiepnhan.Location = New System.Drawing.Point(161, 16)
+        Me.tbmatiepnhan.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbmatiepnhan.Name = "tbmatiepnhan"
         Me.tbmatiepnhan.ReadOnly = True
-        Me.tbmatiepnhan.Size = New System.Drawing.Size(276, 44)
+        Me.tbmatiepnhan.Size = New System.Drawing.Size(185, 32)
         Me.tbmatiepnhan.TabIndex = 3
         '
         'Panel3
@@ -170,36 +179,40 @@ Partial Class tiepnhanxe
         Me.Panel3.Controls.Add(Me.Label5)
         Me.Panel3.Controls.Add(Me.tbmachuxe)
         Me.Panel3.Controls.Add(Me.Label6)
-        Me.Panel3.Location = New System.Drawing.Point(29, 246)
+        Me.Panel3.Location = New System.Drawing.Point(19, 160)
+        Me.Panel3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(998, 183)
+        Me.Panel3.Size = New System.Drawing.Size(665, 119)
         Me.Panel3.TabIndex = 4
         '
         'tbtenchuxe
         '
         Me.tbtenchuxe.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbtenchuxe.Location = New System.Drawing.Point(153, 96)
+        Me.tbtenchuxe.Location = New System.Drawing.Point(102, 62)
+        Me.tbtenchuxe.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbtenchuxe.Name = "tbtenchuxe"
         Me.tbtenchuxe.ReadOnly = True
-        Me.tbtenchuxe.Size = New System.Drawing.Size(317, 39)
+        Me.tbtenchuxe.Size = New System.Drawing.Size(213, 29)
         Me.tbtenchuxe.TabIndex = 8
         '
         'tbdienthoai
         '
         Me.tbdienthoai.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbdienthoai.Location = New System.Drawing.Point(632, 96)
+        Me.tbdienthoai.Location = New System.Drawing.Point(421, 62)
+        Me.tbdienthoai.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbdienthoai.Name = "tbdienthoai"
         Me.tbdienthoai.ReadOnly = True
-        Me.tbdienthoai.Size = New System.Drawing.Size(363, 39)
+        Me.tbdienthoai.Size = New System.Drawing.Size(243, 29)
         Me.tbdienthoai.TabIndex = 7
         '
         'Label8
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(505, 105)
+        Me.Label8.Location = New System.Drawing.Point(337, 68)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(107, 25)
+        Me.Label8.Size = New System.Drawing.Size(77, 17)
         Me.Label8.TabIndex = 6
         Me.Label8.Text = "Điện Thoại"
         '
@@ -207,56 +220,62 @@ Partial Class tiepnhanxe
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(15, 100)
+        Me.Label7.Location = New System.Drawing.Point(10, 65)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(115, 25)
+        Me.Label7.Size = New System.Drawing.Size(80, 17)
         Me.Label7.TabIndex = 4
         Me.Label7.Text = "Tên Chủ xe"
         '
         'tbdiachi
         '
         Me.tbdiachi.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbdiachi.Location = New System.Drawing.Point(632, 21)
+        Me.tbdiachi.Location = New System.Drawing.Point(421, 14)
+        Me.tbdiachi.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbdiachi.Name = "tbdiachi"
         Me.tbdiachi.ReadOnly = True
-        Me.tbdiachi.Size = New System.Drawing.Size(363, 39)
+        Me.tbdiachi.Size = New System.Drawing.Size(243, 29)
         Me.tbdiachi.TabIndex = 3
         '
         'Label5
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(505, 30)
+        Me.Label5.Location = New System.Drawing.Point(337, 19)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(71, 25)
+        Me.Label5.Size = New System.Drawing.Size(51, 17)
         Me.Label5.TabIndex = 2
         Me.Label5.Text = "Địa chỉ"
         '
         'tbmachuxe
         '
         Me.tbmachuxe.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbmachuxe.Location = New System.Drawing.Point(153, 21)
+        Me.tbmachuxe.Location = New System.Drawing.Point(102, 14)
+        Me.tbmachuxe.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbmachuxe.Name = "tbmachuxe"
         Me.tbmachuxe.ReadOnly = True
-        Me.tbmachuxe.Size = New System.Drawing.Size(317, 39)
+        Me.tbmachuxe.Size = New System.Drawing.Size(213, 29)
         Me.tbmachuxe.TabIndex = 1
         '
         'Label6
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(15, 30)
+        Me.Label6.Location = New System.Drawing.Point(10, 19)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(108, 25)
+        Me.Label6.Size = New System.Drawing.Size(74, 17)
         Me.Label6.TabIndex = 0
         Me.Label6.Text = "Mã Chủ xe"
         '
         'btluu
         '
         Me.btluu.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btluu.Location = New System.Drawing.Point(271, 444)
+        Me.btluu.Location = New System.Drawing.Point(181, 289)
+        Me.btluu.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btluu.Name = "btluu"
-        Me.btluu.Size = New System.Drawing.Size(189, 64)
+        Me.btluu.Size = New System.Drawing.Size(126, 42)
         Me.btluu.TabIndex = 5
         Me.btluu.Text = "Lưu"
         Me.btluu.UseVisualStyleBackColor = True
@@ -264,9 +283,10 @@ Partial Class tiepnhanxe
         'btthoat
         '
         Me.btthoat.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btthoat.Location = New System.Drawing.Point(494, 444)
+        Me.btthoat.Location = New System.Drawing.Point(329, 289)
+        Me.btthoat.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btthoat.Name = "btthoat"
-        Me.btthoat.Size = New System.Drawing.Size(189, 64)
+        Me.btthoat.Size = New System.Drawing.Size(126, 42)
         Me.btthoat.TabIndex = 6
         Me.btthoat.Text = "Thoát"
         Me.btthoat.UseVisualStyleBackColor = True
@@ -274,36 +294,29 @@ Partial Class tiepnhanxe
         'tbsolan
         '
         Me.tbsolan.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbsolan.Location = New System.Drawing.Point(1028, 466)
+        Me.tbsolan.Location = New System.Drawing.Point(685, 303)
+        Me.tbsolan.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbsolan.Name = "tbsolan"
         Me.tbsolan.ReadOnly = True
-        Me.tbsolan.Size = New System.Drawing.Size(57, 30)
+        Me.tbsolan.Size = New System.Drawing.Size(39, 23)
         Me.tbsolan.TabIndex = 5
         '
         'labelsolan
         '
         Me.labelsolan.AutoSize = True
         Me.labelsolan.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.labelsolan.Location = New System.Drawing.Point(862, 466)
+        Me.labelsolan.Location = New System.Drawing.Point(575, 303)
+        Me.labelsolan.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.labelsolan.Name = "labelsolan"
-        Me.labelsolan.Size = New System.Drawing.Size(149, 25)
+        Me.labelsolan.Size = New System.Drawing.Size(109, 17)
         Me.labelsolan.TabIndex = 4
         Me.labelsolan.Text = "số lần tiếp nhận"
         '
-        'Label10
-        '
-        Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(834, 30)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(78, 20)
-        Me.Label10.TabIndex = 5
-        Me.Label10.Text = "Thêm xe?"
-        '
         'tiepnhanxe
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1084, 596)
+        Me.ClientSize = New System.Drawing.Size(723, 387)
         Me.ControlBox = False
         Me.Controls.Add(Me.tbsolan)
         Me.Controls.Add(Me.labelsolan)
@@ -313,6 +326,7 @@ Partial Class tiepnhanxe
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "tiepnhanxe"
@@ -353,5 +367,4 @@ Partial Class tiepnhanxe
     Friend WithEvents Label9 As Label
     Friend WithEvents tbsolan As TextBox
     Friend WithEvents labelsolan As Label
-    Friend WithEvents Label10 As Label
 End Class
--- QLGaraGUI/tiepnhanxe.vb
+++ QLGaraGUI/tiepnhanxe.vb
@@ -1,4 +1,5 @@
-﻿Imports QLGaraBus
+﻿
+Imports QLGaraBus
 Imports QLGaraDTO
 Imports Utility
 
@@ -8,9 +9,10 @@ Public Class tiepnhanxe
     Private xBus As xeBus
     Private tnBus As tiepnhanxeBus
     Private hxBus As hieuxeBus
+    Private tsBus As thamsoBus
 
     Private Sub btthoat_Click(sender As Object, e As EventArgs) Handles btthoat.Click
-        If (MessageBox.Show("Thông tin chưa được lưu. Bạn thực sự muốn thoát?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) = DialogResult.OK) Then
+        If (MessageBox.Show(" Bạn thực sự muốn thoát?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) = DialogResult.OK) Then
             Me.Close()
         End If
     End Sub
@@ -20,14 +22,21 @@ Public Class tiepnhanxe
         xBus = New xeBus()
         tnBus = New tiepnhanxeBus()
         hxBus = New hieuxeBus()
+        tsBus = New thamsoBus()
         buimatn()
 
 
         Dim solan As Integer
-        If (solantiepnhan(solan) = True) Then
+        If (solantiepnhan(solan) = 2) Then
             tbsolan.Text = solan.ToString()
+        Else
+            If (solantiepnhan(solan) = 1) Then
+                MessageBox.Show("Đã tiếp nhận đủ xe cho ngày này", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                MessageBox.Show("Đã có lỗi xảy ra", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+            Me.Close()
         End If
-
     End Sub
 
     Private Function buimatn() As Integer
@@ -39,21 +48,35 @@ Public Class tiepnhanxe
         Return 0
     End Function
 
-    Private Function solantiepnhan(ByRef solan) As Boolean
+    Private Function solantiepnhan(ByRef solan) As Integer
         Dim result As Result
         result = tnBus.dem(dtngaytiepnhan.Value, solan)
+
+
         If (result.FlagResult = False) Then
-            Return False
+            Return 0
         Else
-            Return True
+            Dim ts = New thamsoDTO()
+            tsBus.selectall(ts)
+            If (solan >= ts.Soxetoida) Then
+                Return 1
+            End If
+            Return 2
         End If
     End Function
 
     Private Sub dtngaytiepnhan_ValueChanged(sender As Object, e As EventArgs) Handles dtngaytiepnhan.ValueChanged
         buimatn()
         Dim solan As Integer
-        If (solantiepnhan(solan) = True) Then
+        If (solantiepnhan(solan) = 2) Then
             tbsolan.Text = solan.ToString()
+        Else
+            If (solantiepnhan(solan) = 1) Then
+                MessageBox.Show("Đã tiếp nhận đủ xe cho ngày này", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                MessageBox.Show("Đã có lỗi xảy ra", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+            Me.Close()
         End If
     End Sub
 
@@ -63,9 +86,6 @@ Public Class tiepnhanxe
         Dim result As Result
         result = xBus.select_Bybienso(tbbiensoxe.Text, a)
         If (result.FlagResult = False) Then
-            '       Dim b = "Không tìm thấy xe có biển số là " + tbbiensoxe.Text
-            '           MessageBox.Show(b, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            '         System.Console.WriteLine(result.SystemMessage)
             Return False
         Else
             Return True
@@ -87,92 +107,53 @@ Public Class tiepnhanxe
         Return True
     End Function
 
+    Private Sub btluu_Click(sender As Object, e As EventArgs) Handles btluu.Click
+        Dim a As xeDTO
+        a = New xeDTO()
+        If (loadXeByBienso(a) = True) Then
+            Dim x As tiepnhanxeDTO
+            x = New tiepnhanxeDTO()
 
+            '1. Mapping data from GUI control
+            x.Matiepnhan = tbmatiepnhan.Text
+            x.Bienso = tbbiensoxe.Text
+            x.Ngaytiepnhan = dtngaytiepnhan.Value
 
-    Private Sub tbbiensoxe_KeyDown(sender As Object, e As KeyEventArgs) Handles tbbiensoxe.KeyDown
-        If (e.KeyCode = Keys.Enter) Then
-            Dim a As xeDTO
-            a = New xeDTO()
-            If (loadXeByBienso(a) = True) Then
-                Dim b As hieuxeDTO
-                b = New hieuxeDTO()
-                Dim c As chuxeDTO
-                c = New chuxeDTO()
-                loadChuXeByMachuxe(a.Machuxe, c)
-                tbdiachi.Text = c.Diachi
-                tbdienthoai.Text = c.Dienthoai.ToString()
-                tbmachuxe.Text = c.Machuxe
-                tbtenchuxe.Text = c.Tenchuxe
+            Dim b = String.Empty
+            For Each item In x.Bienso
+                b = b + UCase(item)
+            Next
 
+            x.Bienso = b
 
-                loadHieuXeByMahieuxe(a.Mahieuxe.ToString(), b)
-                tbhieuxe.Text = b.Tenhieuxe
+            '2. Insert to DB
+            Dim result As Result
+            result = tnBus.insert(x)
+            If (result.FlagResult = True) Then
+                MessageBox.Show("Tiếp Nhận Xe thành công.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                tbbiensoxe.Text = String.Empty
+                buimatn()
+                Dim solan1 As Integer
+                If (solantiepnhan(solan1) = True) Then
+                    tbsolan.Text = solan1.ToString()
+                End If
+                tbdiachi.Text = String.Empty
+                tbdienthoai.Text = String.Empty
+                tbhieuxe.Text = String.Empty
+                tbmachuxe.Text = String.Empty
+                tbtenchuxe.Text = String.Empty
+
+            Else
+                MessageBox.Show("Tiếp Nhận Xe không thành công.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                System.Console.WriteLine(result.SystemMessage)
+            End If
+        Else
+            If (tbbiensoxe.Text = Nothing) Then
+                MessageBox.Show("Vui lòng nhập biển số", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Else
                 Dim b = "Không tìm thấy xe có biển số là " + tbbiensoxe.Text
                 MessageBox.Show(b, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End If
-
-        End If
-    End Sub
-
-
-    Private Sub btluu_Click(sender As Object, e As EventArgs) Handles btluu.Click
-        Dim solan As Integer
-        If (solantiepnhan(solan) = True) Then
-            If (solan < 30) Then
-                Dim a As xeDTO
-                a = New xeDTO()
-                If (loadXeByBienso(a) = True) Then
-                    Dim x As tiepnhanxeDTO
-
-                    x = New tiepnhanxeDTO()
-
-                    '1. Mapping data from GUI control
-                    x.Matiepnhan = tbmatiepnhan.Text
-                    x.Bienso = tbbiensoxe.Text
-                    x.Ngaytiepnhan = dtngaytiepnhan.Value
-
-                    Dim b = String.Empty
-                    For Each item In x.Bienso
-                        b = b + UCase(item)
-                    Next
-
-                    x.Bienso = b
-
-                    '2. Insert to DB
-                    Dim result As Result
-                    result = tnBus.insert(x)
-                    If (result.FlagResult = True) Then
-                        MessageBox.Show("Tiếp Nhận Xe thành công.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                        tbbiensoxe.Text = String.Empty
-                        buimatn()
-                        Dim solan1 As Integer
-                        If (solantiepnhan(solan1) = True) Then
-                            tbsolan.Text = solan1.ToString()
-                        End If
-                        tbdiachi.Text = String.Empty
-                        tbdienthoai.Text = String.Empty
-                        tbhieuxe.Text = String.Empty
-                        tbmachuxe.Text = String.Empty
-                        tbtenchuxe.Text = String.Empty
-
-                    Else
-                        MessageBox.Show("Tiếp Nhận Xe không thành công.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                        System.Console.WriteLine(result.SystemMessage)
-                    End If
-                Else
-                    If (tbbiensoxe.Text = Nothing) Then
-                        MessageBox.Show("Vui lòng nhập biển số", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    Else
-                        Dim b = "Không tìm thấy xe có biển số là " + tbbiensoxe.Text
-                        MessageBox.Show(b, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    End If
-                End If
-            Else
-                MessageBox.Show("Số lần tiếp nhận trong ngày đã vượt quá con số cho phép", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            End If
-        Else
-            MessageBox.Show("Lấy số lần tiếp nhận trong ngày thất bại", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
     End Sub
 
@@ -189,8 +170,7 @@ Public Class tiepnhanxe
         Dim newf = New themxe()
         newf.ShowDialog(Me)
     End Sub
-
-    Private Sub tbbiensoxe_TextChanged(sender As Object, e As EventArgs) Handles tbbiensoxe.TextChanged
+    Private Sub tbbiensoxe_Leave(sender As Object, e As EventArgs) Handles tbbiensoxe.Leave
         If (tbbiensoxe.Text.Length = 0) Then
             btluu.Enabled = False
         Else
@@ -212,11 +192,5 @@ Public Class tiepnhanxe
                 tbhieuxe.Text = b.Tenhieuxe
             End If
         End If
-
-    End Sub
-
-    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
-        Dim newf = New themhieuxe()
-        newf.ShowDialog(Me)
     End Sub
 End Class
